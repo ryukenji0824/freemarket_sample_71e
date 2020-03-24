62ee82c3927048fdc233ff03e46f25fbb3fe12d4
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-03-24 19:36:17 +0900

    userの変更一覧ページに行けるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:index, :show, :edit]
+  before_action :set_user, only: :show
 
   def index
+    @user = User.find(current_user.id)
   end
 
   def show
   end
 
-  def edit
-  end
 
   private
   def set_user
     @user = User.find(params[:id])
   end
+
+  def user_params
+    params.require(user).permit(:nickname, :email, :introduction, :password, :password_confirmation)
+  end
+
 
 end
