157c26fa518e6299f3d8fc001d80f9ab64fb2368
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-03-25 14:27:45 +0900

    プロフィールを関係

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,35 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: :show
+  before_action :set_user_params, only: [:show, :edit, :update]
+  before_action :set_user_current, only: :index
 
   def index
-    @user = User.find(current_user.id)
   end
 
   def show
   end
 
+  def edit
+  end
+
+  def update
+    @user.update(update_params)
+    sign_in(:user, @user)
+  end
+
 
   private
-  def set_user
+  
+  def set_user_params
     @user = User.find(params[:id])
   end
 
-  def user_params
-    params.require(user).permit(:nickname, :email, :introduction, :password, :password_confirmation)
+  def set_user_current
+    @user = User.find(current_user.id)
+  end
+
+  def update_params
+    params.require(:user).permit(:nickname, :introduction, :password, :password_confirmation)
   end
 
 
