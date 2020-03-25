215bebba7b1c1cb4ed3b19777f224bbdcb75ba3d
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-03-25 17:55:20 +0900

    gemの追加とカードテーブルの編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200324064341) do
+ActiveRecord::Schema.define(version: 20200325085315) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "family_name",      null: false
@@ -36,10 +36,11 @@ ActiveRecord::Schema.define(version: 20200324064341) do
   end
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "token",      limit: 65535, null: false
-    t.integer  "user_id",                  null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "customer_id", null: false
+    t.string   "card_id",     null: false
     t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
   end
 
