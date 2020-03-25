0bd8973bc45a030f829d60e27c011d5b8a70ff9e
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-03-25 17:05:12 +0900

    addressコントローラーのリファクタリング

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,10 @@
 class AddressesController < ApplicationController
+  before_action :set_user_params, only: [:edit, :update]
   def edit
-    @user = User.find(params[:id])
     @address = @user.address
   end
 
   def update
-    @user = User.find(params[:id])
     @address = @user.address
     @address.update(update_params)
     sign_in(:user, @user)
@@ -14,5 +13,9 @@ class AddressesController < ApplicationController
   private
   def update_params
     params.require(:address).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :city, :local, :block, :building, :number)
+  end
+
+  def set_user_params
+    @user = User.find(params[:id])
   end
 end
