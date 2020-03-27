8319c34aa0ef23874607d742e9291983e9873ed1
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-03-27 16:37:17 +0900

    カード登録後のviewファイルの指定の変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -17,9 +17,7 @@ class CardsController < ApplicationController
       metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to action: "show"
-      else
+      unless @card.save
         redirect_to action: "pay"
       end
     end
