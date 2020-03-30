6c8aff91a32af54dae0e6528deac6e3a675760db
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-03-30 15:01:36 +0900

    routingを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user_params, only: [:show, :edit, :update]
-  before_action :set_user_current, only: :index
+  before_action :set_user_current, only: [:index, :edit_detail, :update_detail]
 
   def index
     @items = @user.items
@@ -22,6 +22,17 @@ class UsersController < ApplicationController
     sign_in(:user, @user)
   end
 
+  def edit_detail
+  end
+
+  def update_detail
+    @user.update(update_detail_params)
+    unless @user.update(update_detail_params)
+      redirect_to user_edit_detail_path(@user.id), notice: "変更できません。ニックネームとパスワードは必須項目です"
+    end
+    sign_in(:user, @user)
+  end
+
 
   private
   
@@ -35,6 +46,10 @@ class UsersController < ApplicationController
 
   def update_params
     params.require(:user).permit(:nickname, :introduction, :password, :password_confirmation, :user_image)
+  end
+
+  def update_detail_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 
