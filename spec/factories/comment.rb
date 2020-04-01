b82c34b2607f447805181c6c36f575a180f9db3e
takuro627 <t.i.ryuf14@gmail.com>
2020-04-01 15:18:42 +0900

    factory_botを用いたテスト

---
+++ spec/factories/comment.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :comment do
+    id                       {"1"}
+    user_id                  {"2"}
+    item_id                  {"3"}
+    text                     {"test"}
+  end
+
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,5 +16,20 @@ describe Comment do
       comment.valid?
       expect(comment.errors[:item_id]).to include("を入力してください")
     end
+    it "is invalid without a text" do
+      comment = build(:comment, text: "")
+      user.valid?
+      expect(comment.errors[:text]).to include("を入力してください")
+    end
+    it "is invalid without a user_id" do
+      comment = build(:comment, user_id: "")
+      comment.valid?
+      expect(comment.errors[:user_id]).to include("を入力してください")
+    end
+    it "is invalid without a item_id" do
+      comment = build(:comment, item_id: "")
+      comment.valid?
+      expect(comment.errors[:item_id]).to include("を入力してください")
+    end
   end
 end
