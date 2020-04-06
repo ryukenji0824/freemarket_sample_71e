964801fec3abb6ede8eddd9f605a46ed52212b59
takuro627 <t.i.ryuf14@gmail.com>
2020-04-06 13:25:35 +0900

    カテゴリー一覧表示を新着順に修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
 
   def show 
     @category = Category.find(params[:id])
-    @items = @category.items.page(params[:page]).per(5)
+    @items = @category.items.order('created_at DESC').page(params[:page]).per(5)
   end
 end
